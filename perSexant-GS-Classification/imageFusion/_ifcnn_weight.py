3a4a52aa23ed4fb492700b52ed7955c647af06ab
amirali <amirali.tj45@gmail.com>
2026-07-04 13:25:15 +0330

    fixed

--- perSexant-GS-Classification/imageFusion/_ifcnn_weight.py
+++ perSexant-GS-Classification/imageFusion/_ifcnn_weight.py
@@ -1,24 +1,54 @@
 import torch
+import tensorflow as tf
 import torchvision.models as models
 import numpy as np
 from pprint import pprint
+import collections
+
+class kerasWeightsConstruct :
+    def __init__(self , statDict : collections.OrderedDict):
+        self.stat = statDict
+        self._sampleLayerNames()
+
+    def kerasWeightConstructor(self) :
+        for layer , value in self.stat.items() :
+            if np.size(value.shape) == 4 :
+                self.stat[layer] = np.transpose(
+                    value ,
+                    axes=(2 , 3 , 1 , 0)
+                )
+        return self.stat
+
+    def _sampleLayerNames(self) :
+        names = [
+            "conv1_weight" , 
+            "conv2_weight" ,
+            "conv3_weight" ,
+            "conv4_weight" ,
+            "conv4_bias" ,
+            "conv2_bn_weight" ,
+            "conv2_bn_bias" ,
+            "conv2_bn_running_mean" ,
+            "conv2_bn_running_var" ,
+            "conv3_bn_weight" ,
+            "conv3_bn_bias" ,
+            "conv3_bn_running_mean" ,
+            "conv3_bn_running_var"
+        ]
+        print("example dict layer names : ")
+        pprint(names)   
 
 
 
-resnet = models.resnet101(pretrained=True)
-torch_weights = np.array(resnet.conv1.weight.data) # channel first
-torch_bias    = np.array(resnet.conv1.bias) 
 
-Conv1W = np.transpose(
-    torch_weights ,
-    axes = (2 , 3 , 1 , 0)
+#--- run test  
+model = torch.load(
+    "fusionWeights/IFCNN-MAX.pth" , 
+    map_location=torch.device('cpu')
 )
 
-channel_last_weights = {
-    "conv1" : Conv1W
-}
+convert = kerasWeightsConstruct(model)
+weights = convert.kerasWeightConstructor()
+for layer , value in weights.items() :
+    print(layer , " :" , value.shape)
 
-# some are bias true and some are bias false
-#layers = dict(resnet.named_children())
-#print(layers["layer1"][0]) 
-# next ....
