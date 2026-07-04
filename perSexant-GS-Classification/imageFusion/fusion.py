3c344527a09d938f69f705b9ca0bab6f56773698
amirali <amirali.tj45@gmail.com>
2026-07-04 13:26:39 +0330

    .build method fixed

--- perSexant-GS-Classification/imageFusion/fusion.py
+++ perSexant-GS-Classification/imageFusion/fusion.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
 import keras
-from keras.layers import Conv2D , ReLU , BatchNormalization , ZeroPadding2D
-from _ifcnn_weight import channel_last_weights
+from keras.layers import Conv2D , ReLU , BatchNormalization , ZeroPadding2D , Average
+#from _ifcnn_weight import channel_last_weights
 
 # implemet the IFCNN model in keras and load weights on the model
 
 class ConvBlock(keras.layers.Layer) :
     def __init__(self) :
         super().__init__()
-        self.Conv2D    = Conv2D(filters=64 , kernel_size=7 , padding="same" , strides=1) # padding 3
+        self.Conv2D    = Conv2D(filters=64 , kernel_size=3 , padding="same" , strides=1)
         self.Relu      = ReLU()
         self.batchnorm = BatchNormalization()
     def call(self , tensor):
@@ -19,27 +19,44 @@ class ConvBlock(keras.layers.Layer) :
         return x
     
 class imageFusionCNN :
-    def __init__(self , fuseType=None , data_format="channel_last") :
+    def __init__(self , fuseType="MEAN" , data_format="channel_last") :
         self.type   = fuseType
         self.format = data_format
     
     def build(self) :
         # model layers
         if self.format == "channel_last" :
-            inpt      = keras.Input(shape=(224 , 224 , 3))
+            inpt_1      = keras.Input(shape=(224 , 224 , 3))
+            inpt_2      = keras.Input(shape=(224 , 224 , 3))
         elif self.format == "channel_first" :
-            inpt      = keras.Input(shape=(3 , 224 , 224))
-        
+            inpt_1      = keras.Input(shape=(3 , 224 , 224))
+            inpt_2      = keras.Input(shape=(3 , 224 , 224))
         ZeroPad2D = ZeroPadding2D(padding=((3 , 3) , (3 , 3)))
         Conv1     = Conv2D(filters=64, kernel_size=7 , strides=1 , name="conv1")
+        Conv2     = ConvBlock()
+        Conv3     = ConvBlock()
+        Conv4     = Conv2D(filters=3, kernel_size=1 , name="conv4")
+        average   = Average() 
 
         def forward() :
-            x = ZeroPad2D(inpt)
-            x = Conv1(x)
-            return x
+            # image 1
+            x1 = ZeroPad2D(inpt_1)
+            x1 = Conv1(x1)
+            x1 = Conv2(x1)
+
+            # image 2
+            x2 = ZeroPad2D(inpt_2)
+            x2 = Conv1(x2)
+            x2 = Conv2(x2)
+
+            # merge 
+            mrgX = average([x1 , x2])
+            mrgX = Conv3(mrgX)
+            mrgX = Conv4(mrgX)
+            return mrgX
         
         model = keras.Model(
-            inputs  = inpt ,
+            inputs  = [inpt_1 , inpt_2] ,
             outputs = forward()
         )
         self._model = model
@@ -55,6 +72,5 @@ class imageFusionCNN :
 
 fuse = imageFusionCNN()
 fuse.build()
-fuse.load_weights(channel_last_weights)
 model = fuse.get_model()
 model.summary()
