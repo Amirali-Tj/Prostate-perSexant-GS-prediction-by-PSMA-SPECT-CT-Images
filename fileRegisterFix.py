deaaf486b8b27e11663a02297857e39c395c9f23
amirali <amirali.tj45@gmail.com>
2026-07-08 17:36:12 +0330

    spect images fixed

--- fileRegisterFix.py
+++ fileRegisterFix.py
@@ -1,1 +1,20 @@
-# to be continued ...
+import os
+import nibabel as nib
+import numpy as np
+
+
+mainPath = "PSMA nii"
+for name in os.listdir(mainPath) :
+    if name != ".DS_Store" :
+        pFolder = os.path.join(mainPath , name)
+        for file in os.listdir(os.path.join(pFolder , "nii files")) :
+            if "Oncology" in file and "nii" in file :
+
+                spect_file = nib.load(os.path.join(pFolder , "nii files" , file))
+                array      = spect_file.get_fdata()
+                array      =  np.flip(
+                    array ,
+                    axis=2
+                )
+                spect_obj = nib.Nifti1Image(array , affine=spect_file.affine , header=spect_file.header)
+                nib.save(spect_obj , os.path.join(pFolder , "nii files" , file))
