a5de00fc0a3fd0cabcdff60b95b2d288620c3cd8
amirali <amirali.tj45@gmail.com>
2026-07-17 23:08:40 +0330

    origin fixed

--- fileRegisterFix.py
+++ fileRegisterFix.py
@@ -11,10 +11,21 @@ for name in os.listdir(mainPath) :
             if "Oncology" in file and "nii" in file :
 
                 spect_file = nib.load(os.path.join(pFolder , "nii files" , file))
+
+                # array fix
                 array      = spect_file.get_fdata()
                 array      =  np.flip(
                     array ,
                     axis=2
                 )
+
+                # origin fix on K dim
+                spacing_k = spect_file.affine[2][2]
+                origin_k  = spect_file.affine[2][3]
+                originFix = origin_k - (127*spacing_k) # spect matrix 128*128
+                spect_file.affine[2][3] = originFix
+                print(spect_file.affine)
+
+                # save fixed spect
                 spect_obj = nib.Nifti1Image(array , affine=spect_file.affine , header=spect_file.header)
                 nib.save(spect_obj , os.path.join(pFolder , "nii files" , file))
